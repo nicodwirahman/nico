630cf5d75889d2ba8f341782bbf4e76c79d71536
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-03-06 15:39:04 +0700

    fisrt commit

---
+++ example1.cpp
@@ -0,0 +1,24 @@
+//library
+#include <iostream>
+using namespace std;
+
+//program main
+int main(){//begin
+
+//numeric p,l,luas
+double p,l,luas;
+//display "masukan panjang"
+cout<<"masukan panjang=";
+//accept p
+cin>>p;
+//display "masukan nama"
+cout<<"masukan lebar=";
+//accept L
+cin>>l;
+//compute Luas=p*L
+luas=p*l;
+//display "luas persegi panjang adalah",luas
+cout<<"luas persegi panjang"<<luas;
+
+
+}//end
